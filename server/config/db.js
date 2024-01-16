8a8d0fe9127cdc967b3bf4267c9c42a1c2c09402
kbyogesh <yogeshkumarb1818@gmail.com>
2024-01-17 00:43:02 +0530

    first commit

---
+++ server/config/db.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const connectDB = async () => {
+
+  try{
+    mongoose.set('strictQuery', false);
+    const conn =  await mongoose.connect(process.env.MONGODB_URI);
+    console.log(`database connected : ${conn.connection.host}`);
+  }catch (error){
+    console.log(error);
+  }
+}
+
+// very important
+ 
+module.exports = connectDB;
